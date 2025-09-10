308a5d62943d7f94bb207ca917313ca3b51cb804
niki <nikihidayati20@gmail.com>
2025-09-10 22:50:51 +0800

    Initial Commit

---
+++ anima/animaApp.swift
@@ -0,0 +1,17 @@
+//
+//  animaApp.swift
+//  anima
+//
+//  Created by Niki Hidayati on 10/09/25.
+//
+
+import SwiftUI
+
+@main
+struct animaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ animaTests/animaTests.swift
@@ -0,0 +1,17 @@
+//
+//  animaTests.swift
+//  animaTests
+//
+//  Created by Niki Hidayati on 10/09/25.
+//
+
+import Testing
+@testable import anima
+
+struct animaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
